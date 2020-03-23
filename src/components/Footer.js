0f452df0c94fd4214b8a275e1197752df3bd3dbd
Chris Noods <bakernoodles@hotmail.com>
2020-03-23 01:49:09 -0700

    bug fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../componentStyles/footer.css';
-import image from '/programming/react-apps/markdown-previewer/src/components/github.png';
+import image from '../components/github.png';
 const footer = () => {
     return <footer className="footer">
         <a href='https://github.com/bakernoodles' target="_blank" ><img alt='Github Logo' src={image}/></a>
